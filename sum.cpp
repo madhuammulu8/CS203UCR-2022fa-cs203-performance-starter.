aedfb6f8b3cbbecb9c99e804276692624df1648c
madhuammulu8 <madhuammulu8@gmail.com>
2022-10-13 10:59:13 -0700

    My progress so far.

--- sum.cpp
+++ sum.cpp
@@ -11,19 +11,37 @@
 #define END_C  }
 
 
+// START_C
+
+// uint64_t sum_of_locations_solution(uint64_t *search_space, uint32_t search_space_size, uint64_t* queries, uint32_t query_count)
+// {
+// 	uint64_t r = 0;
+
+// 	for(uint32_t i = 0; i < query_count; i++) {
+// 		for(uint32_t j = 0; j < search_space_size; j++) {
+// 			if (search_space[j] == queries[i]) {
+// 				r += i;
+// 				break;
+// 			}
+// 		}
+// 	}
+// 	return r;
+// }
+
+// END_C
+
 START_C
 
 uint64_t sum_of_locations_solution(uint64_t *search_space, uint32_t search_space_size, uint64_t* queries, uint32_t query_count)
 {
 	uint64_t r = 0;
+    std::unordered_set<int> s(search_space, search_space + search_space_size);
 
-	for(uint32_t i = 0; i < query_count; i++) {
-		for(uint32_t j = 0; j < search_space_size; j++) {
-			if (search_space[j] == queries[i]) {
+		for(uint32_t i = 0; i < query_count; i++) {
+			if (s.find(queries[i]) == s.end()) {
 				r += i;
 				break;
 			}
-		}
 	}
 	return r;
 }
